d6c96c305b05f7e4d3bca0fec325cd8d0032d062
Jan-Markus Langer <hallo@janmarkuslanger.de>
2020-01-03 22:54:10 +0100

    generate

---
+++ src/generate.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import os
+import sys
+
+NOT_REMOVE = ('.git', 'src', '.gitattributes', '.gitignore', 'generate.py')
+
+os.system('cd src && npm run generate')
+os.system('mv src/dist/* ../*')
+
+for file in os.listdir('./'):
+    if file not in NOT_REMOVE:
+        # os.remove('./{}'.format(file))
+        print(file)
