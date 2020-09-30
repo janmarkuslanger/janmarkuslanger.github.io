a37e6adbcf1a8916466d7c3057660f2ced4fb93c
Jan-Markus Langer <janmarkuslanger10121994@gmail.com>
2020-09-30 15:54:55 +0000

    Add navigation component

---
+++ components/navigation.vue
@@ -0,0 +1,24 @@
+<template>
+    <nav class="navigation">
+        <div class="navigation__inside">
+
+            <ul class="navigation__links">
+
+                <li class="navigation__item"><nuxt-link class="navigation__link" to="/projects">Projects<nuxt-link></li>
+                <li class="navigation__item"><nuxt-link class="navigation__link" to="/contact">Contact<nuxt-link></li>
+
+            </ul>
+
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    
+};
+</script>
+
+<style lang="scss">
+
+</style>
