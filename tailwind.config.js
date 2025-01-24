ac42cdb833d3a344d7172b600e81a3981442d269
janmarkuslanger <janmarkuslanger10121994@gmail.com>
2025-01-24 23:12:18 +0100

    add imprint & datasecure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-    content: ["./index.html",  "./src/**/*.{js,ts}"],
+    content: ["./*.html", "./src/**/*.{js,ts}"],
     theme: {
       extend: {},
     },
